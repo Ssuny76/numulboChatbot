75a7446e79d1773d0cde8f728ca709fbe670d342
jc <luvimperfection@gmail.com>
2018-11-16 17:45:00 +0900

    updated verify token

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+  const VERIFY_TOKEN = 'VERIFY_TOKEN';
 
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
