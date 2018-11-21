ca9cd5f050970d3eaad5853f1a61f32d39339d2f
jc <luvimperfection@gmail.com>
2018-11-21 15:07:59 +0900

    길이야 출력되어라

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ function receivedMessage(event) {
 function productSearchMessage(recipientId, productName){
      var sqlquery = 'select *, (score/char_length(item_name)) as accuracy from (select *, if((instr(item_name, "불"))=0, 0, 1)+if((instr(item_name, "닭"))=0, 0, 1)+if((instr(item_name, "볶"))=0, 0, 1)+if((instr(item_name, "음"))=0, 0, 1)+if((instr(item_name, "면"))=0, 0, 1) as score from stores1.item_table) as A order by score desc, accuracy desc limit 4;';
      var resultItem = [];
-     var resultLength;
+     var resultLength=0;
 
     var getInformationFromDB = function(callback) {
        connection.query(
