c564a7e88e555fa215ed6b161f2f977c3fab9be1
jc <luvimperfection@gmail.com>
2018-11-22 13:25:48 +0900

    주석 풀었어

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ function receivedMessage(event) {
     if (coordinates && !isNaN(coordinates.lat) && !isNaN(coordinates.long)){
         
 
-        /*
+        
         var locationSQL = 'INSERT INTO stores1.userData(userId, lat, long, time)VALUES(?,?,?,?);';
         connection.query(locationSQL, [senderId , coordinates.lat, coordinates.long, event.timestamp], function (err, data) {
             if (err) {
@@ -125,7 +125,7 @@ function receivedMessage(event) {
                 console.log("sql에 들어갔당");
             }
         });
-        */
+        
 
         console.log("timestamp looks like - "+event.timestamp);
 
@@ -175,11 +175,100 @@ function receivedMessage(event) {
 
 
 function productSearchMessage(recipientId, productName){
+  // 님들 이거 아이템 하나 딱 뜰때 list처리해줘야함
 
+    var input_array = productName.split(' ');
+    console.log(input_array);
+    var input_concat = productName.replace(' ','');
+    console.log(input_concat);
+    var input_char = input_concat.split('');
+    console.log(input_char);
 
+    var sqlquery = 'select *, (score/char_length(item_name)) as accuracy from (select *, if((instr(item_name, "';
+    for (var i=0; i<input_char.length-1; i++){
+      sqlquery += input_char[i]+'"))=0, 0, 1)+if((instr(item_name, "';
+    }
+    sqlquery += input_char[input_char.length-1]+'"))=0, 0, 1) as score from stores1.item_table) as A order by score desc, accuracy desc limit 4;'
 
+    console.log(sqlquery);
 
+    var resultItem = [];
 
+    var tempElement1 = {
+        "buttons": [
+              {
+                type: "postback"
+              }
+            ]
+      };
+
+      var tempElement2 = {
+        "buttons": [
+              {
+                type: "postback"
+              }
+            ]
+      };
+
+      var tempElement3 = {
+        "buttons": [
+              {
+                type: "postback"
+              }
+            ]
+      };
+
+      var tempElement4 = {
+        "buttons": [
+              {
+                type: "postback"
+              }
+            ]
+      };
+
+      var tempElements = [];
+      tempElements.push(tempElement1);
+      tempElements.push(tempElement2);
+      tempElements.push(tempElement3);
+      tempElements.push(tempElement4);
+      var response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements":[]
+          }
+        }
+      };
+
+    var getInformationFromDB = function(callback) {
+       connection.query(
+        sqlquery,
+        function(err, results, fields){
+          if(err) throw err;
+          callback(results);
+
+        }
+     );
+     };
+
+      getInformationFromDB(function (results) {
+          resultItem = results;
+          for(var i=0; i<resultItem.length; i++){
+            tempElements[i].title = resultItem[i].item_name;
+            tempElements[i].image_url = resultItem[i].img_src;
+            tempElements[i].buttons[0].title = resultItem[i].item_name;
+            tempElements[i].buttons[0].payload = CVSinfo;
+            response.attachment.payload.elements.push(tempElements[i]);
+          };
+          sendTextMessage(recipientId, response);
+        }
+      );
+     return;
+}
+
+function cvsSearchMessage(recipientId, productName){
   // 님들 이거 아이템 하나 딱 뜰때 list처리해줘야함
 
     var input_array = productName.split(' ');
@@ -271,9 +360,15 @@ function productSearchMessage(recipientId, productName){
         }
       );
 
-     
-     return;
+      var CVSinfoPMESSAGE = "해당 상품의 재고가 있는 매점을 가까운 순으로 찾는 중입니다";
+      var CVSinfoPAYLOAD = {
+        "text": CVSinfoPMESSAGE
+    }
+      sendTextMessage(senderID, CVSinfoPAYLOAD);
 
+
+
+     return;
 }
 
 function receivedPostback(sender_psid, received_postback) {
@@ -310,11 +405,23 @@ function receivedPostback(sender_psid, received_postback) {
       sendTextMessage(senderID, noPayload);
       break;
     case CVSinfo:
-      var CVSinfoPMESSAGE = "찾으시는 상품의 재고가 있는 매점은 다음과 같습니다.";
-      var CVSinfoPAYLOAD = {
-        "text": CVSinfoPMESSAGE
-    }
-      sendTextMessage(senderID, CVSinfoPAYLOAD);
+      const selectedName = received_postback.title;  
+      //제품명 저장 쿼리
+// 선택한 제품명을 userData에 추가하려고 함.
+// 
+
+
+      var productSave = 'UPDATE stores1.userData SET product = '+selectedName+'WHERE userId ='+senderID+' AND userId = temp.userId FROM temp as SELECT userID, max(stores1.userData.timestamp) FROM stores1.userData;';
+      connection.query(productSave, [senderId , content], function (err, data) {
+          if (err) {
+              console.log("sql에 저장하지 못했다고합니다..");
+          } else {
+              console.log("sql에 들어갔당");
+          }
+      });
+
+      // 편의점 찾는 함수로 연결
+      cvsSearchMessage(sender_psid, selectedName)
       break;
     case Help:
     break;
